c7b8de8861c8a669dea81a2807bd19a6c5c9f304
Karol Słuszniak <ksluszniak@gmail.com>
2018-01-11 17:10:38 +0100

    Tweak circle config with artifact and test result

--- mix.exs
+++ mix.exs
@@ -28,6 +28,7 @@ defmodule Flaggy.Mixfile do
       {:credo, "~> 0.8", only: [:dev, :test]},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       {:excoveralls, "~> 0.7", only: :test},
+      {:junit_formatter, "~> 2.1", only: :test},
       {:protein, "~> 0.10", optional: true},
       {:recon, "~> 2.3", optional: true},
       {:poison, ">= 1.0.0"}
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,3 @@
+File.mkdir_p(Application.get_env(:junit_formatter, :report_dir))
+ExUnit.configure formatters: [JUnitFormatter, ExUnit.CLIFormatter]
 ExUnit.start()
